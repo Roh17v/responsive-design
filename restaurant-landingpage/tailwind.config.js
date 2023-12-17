eda4c93cdb0f111ab61b84c9fe09e081545e93f9
Rohit Verma <137288929+Roh17v@users.noreply.github.com>
2023-12-17 14:17:22 +0000

    Fully Responsive Restaurant landing page

---
+++ restaurant-landingpage/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "Open-Sans": ["Open Sans", "sans-serif"],
+        "Playfair": ["Playfair Display", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
